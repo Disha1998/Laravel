7a680a2b4c6b62dc05b088bbe340d1d3dacce672
Disha Sathavara <dishasathavara.com>
2021-07-03 03:07:23 +0530

    Laravel using Firebase database

---
+++ resources/views/login.blade.php
@@ -0,0 +1,6 @@
+<h1>User Login</h1>
+<form action="" method="POST">
+<input type="text" name="user" placeholder="Enter Your Name"><br><br>
+<input type="password" name="Password" placeholder="Enter Your Password"><br><br>
+<button type="submit"> Login</button>
+</form>
